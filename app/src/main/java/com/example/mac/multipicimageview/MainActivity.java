eaeb9708892ad6b5edaff3933b33dd7bc2514afa
allen218 <wangcongccnp@126.com>
2016-04-20 10:13:13 +0800

    添加4按钮展示

--- app/src/main/java/com/example/mac/multipicimageview/MainActivity.java
+++ app/src/main/java/com/example/mac/multipicimageview/MainActivity.java
@@ -5,25 +5,138 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import com.jakewharton.rxbinding.view.RxView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
     private MultiPicImageView mPic;
+
+    @Bind(R.id.four_btn)
+    Button four_btn;
+
+    @Bind(R.id.five_btn)
+    Button five_btn;
+
+    @Bind(R.id.six_btn)
+    Button six_btn;
+
+    @Bind(R.id.nine_btn)
+    Button nine_btn;
+
+    private List<Bitmap> mBitmapList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
+
         initView();
+
+        initRxBinding();
 
     }
 
     private void initView() {
         mPic = (MultiPicImageView) findViewById(R.id.iv);
 
+    }
+
+    private void initRxBinding() {
+        RxView.clicks(four_btn).subscribe(this::clickFourBtn);
+        RxView.clicks(five_btn).subscribe(this::clickFiveBtn);
+        RxView.clicks(six_btn).subscribe(this::clickSixBtn);
+        RxView.clicks(nine_btn).subscribe(this::clickNineBtn);
+    }
+
+    /**
+     * 点击4张图片
+     *
+     * @param oVoid
+     */
+
+    private void clickFourBtn(Void oVoid) {
+        createShowData(4);
+    }
+
+    /**
+     * 点击5张图片
+     *
+     * @param oVoid
+     */
+    private void clickFiveBtn(Void oVoid) {
+        createShowData(5);
+
+    }
+
+    /**
+     * 点击6张图片
+     *
+     * @param oVoid
+     */
+    private void clickSixBtn(Void oVoid) {
+        createShowData(6);
+
+    }
+
+    /**
+     * 点击9张图片
+     *
+     * @param oVoid
+     */
+    private void clickNineBtn(Void oVoid) {
+
+        createShowData(9);
+    }
+
+    /**
+     * 创建图片数据
+     *
+     * @param num
+     */
+    private void createShowData(int num) {
+        if (mBitmapList == null) {
+            mBitmapList = new ArrayList<>();
+        } else {
+            if (mBitmapList.size() > num) {
+                removeListData(mBitmapList, num);
+            } else {
+                addListData(mBitmapList, num);
+            }
+        }
+        mPic.addImg(mBitmapList);
+    }
+
+    /**
+     * 移除List元素
+     *
+     * @param mBitmapList
+     * @param end
+     */
+    private void removeListData(List<Bitmap> mBitmapList, int end) {
+        for (int x = end; x < mBitmapList.size(); x++) {
+            mBitmapList.remove(x);
+        }
+    }
+
+    /**
+     * 添加元素
+     *
+     * @param mBitmapList
+     * @param end
+     */
+    private void addListData(List<Bitmap> mBitmapList, int end) {
+        for (int x = mBitmapList.size(); x < end; x++) {
+            mBitmapList.add(BitmapFactory.decodeResource(getResources(), R.drawable.a2));
+        }
     }
 
     @Override
@@ -33,7 +146,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initData() {
-        final List<Bitmap> mData = createData();
+        /*final List<Bitmap> mData = createData();
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -45,7 +158,7 @@ public class MainActivity extends AppCompatActivity {
                 mPic.addImg(mData);
 
             }
-        }).start();
+        }).start();*/
     }
 
     @NonNull
